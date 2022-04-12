9f669d409a46290dcc5f4e8428111c8fecb3b36d
Chanwoo <tree698@gmail.com>
2022-04-12 11:13:54 +0900

    add card and edit form component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import CardEditForm from '../card_edit_form/card_edit_form';
-import style from './editor.module.css';
+import styles from './editor.module.css';
 
 const Editor = ({ cards }) => (
-  <section className={style.editor}>
-    <h1 className={style.title}>Card Maker</h1>
+  <section className={styles.editor}>
+    <h1 className={styles.title}>Card Maker</h1>
     {cards.map((card) => (
       <CardEditForm card={card} />
     ))}
---
+++ src/components/image_file_input/imageFileInput.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './imageFileInput.module.css';
+
+const ImageFileInput = (props) => <button>Image</button>;
+
+export default ImageFileInput;
