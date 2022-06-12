c829b1fabb6f0a0b7f02d083e73c8475940f0f0e
Chanwoo <tree698@gmail.com>
2022-06-12 19:23:28 +0900

    change Cloudinary API key

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -2,9 +2,9 @@ class ImageUploader {
   async upload(file) {
     const data = new FormData();
     data.append('file', file);
-    data.append('upload_preset', 'pdzaoz52');
+    data.append('upload_preset', 'hwmq0jxv');
     const result = await fetch(
-      'http://api.cloudinary.com/v1_1/drqni4rhj/upload',
+      'http://api.cloudinary.com/v1_1/dea5jeo2i/upload',
       {
         method: 'POST',
         body: data,
